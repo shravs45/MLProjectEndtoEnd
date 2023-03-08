8f1b4f5742993f8214a1b5b73ffc3d874babc3fb
Shravanthi <shravs45@gmail.com>
2023-03-08 23:16:49 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='shravanthi',
+    author_email='',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+)
